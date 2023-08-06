cfedbc136bb53b60ee66f938e37996aaf06404c9
Ethan denton <ethanjdenton@gmail.com>
2023-08-06 16:52:40 +0100

    funny pokemon thing

---
+++ react_thingy/src/App.js
@@ -0,0 +1,54 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+
+function App() {
+  const [count, setcount] = useState();
+  const [pokemon, setpokemon] = useState([]);
+  const [search, setsearch] = useState();
+
+
+  function fetchKantoPokemon(){
+    setpokemon('')
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${search}`)
+    .then(response => response.json())
+    .then(allpokemon => setpokemon(allpokemon))
+    console.log(pokemon)
+  }
+
+  useEffect(() => {
+    setcount(0)
+  },[])
+
+  return (
+    <div className="App">
+      <button onClick={() => setcount(count+1)}>+</button>
+      <h1>{count}</h1>
+      <button onClick={() => setcount(count-1)}>-</button>
+      <input 
+      placeholder='Search'
+      value={search}
+      onChange={(e) => setsearch(e.target.value)}
+      />
+      <button onClick={fetchKantoPokemon}>Poekemon</button>
+
+      {
+        pokemon.name?.length > 0
+          ? (
+            <h1 className='container'>
+              {pokemon.name}<br />
+              <img src={pokemon.sprites.versions["generation-ii"].crystal.front_default} />
+            </h1>
+          ) : (
+            <div className='empty'>
+              <h2>No Pokemon Found</h2>
+            </div>
+          )
+      }
+
+    </div>
+  );
+}
+
+export default App;
