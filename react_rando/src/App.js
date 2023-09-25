8b5b547dcd937f989498ba0725ea61369b88c236
Ethan denton <ethanjdenton@gmail.com>
2023-09-25 19:10:15 +0100

    weather api

--- react_rando/src/App.js
+++ react_rando/src/App.js
@@ -5,7 +5,9 @@ import Homecard from './Homecard'
 function App() {
   const [nav, setnav] = useState()
   const [nasa, setnasa] = useState([])
+  const [weather, setweather] = useState([])
   const NASA_API = 'aNPUGKBJgz847fa29nRAkUe01yQlQeCl5Nb1EbIe'
+  const W_API = 'ea0c3ac899f343c1b5e173401232509'
 
   function fectchNasa() {
     fetch(`https://api.nasa.gov/planetary/apod?api_key=${NASA_API}`)
@@ -13,8 +15,16 @@ function App() {
     .then(data => setnasa(data))
   }
 
-  useEffect(()=>setnav(1),[])
+  function fetchweather() {
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=ea0c3ac899f343c1b5e173401232509&q=Bristol&days=7&aqi=no&alerts=no1`)
+    .then(response => response.json())
+    .then(weather => setweather(weather.forecast.forecastday))
+    console.log(weather)
+  }
+
+  useEffect(()=>setnav(2),[])
   useEffect(fectchNasa,[])
+  useEffect(fetchweather,[])
 
 
 
@@ -38,13 +48,34 @@ function App() {
           ? (
             <div className='one-home'>
               <Homecard prop={nasa} />
+              <div>
+                hello
+              </div>
             </div>
           ) :
-            console.log()
+            console.log(weather)
         }
         {nav === 2
           ? (
-            <div className='two-home'>two</div>
+            <div>
+              <div className='two-home'>
+                two
+              </div>
+              <div className='weather-container'>
+                {weather.map((e)=>(
+                    <div className='weather'>
+                      <span className='weather-date'>{e.date}</span>
+                      <img src={e.day.condition.icon} />
+                      <span className='weather-temp'>
+                        {e.day.avgtemp_c}°C
+                      </span><br />
+                      <span className='weather-temprange'>
+                      { e.day.maxtemp_c}-{e.day.mintemp_c}
+                      </span>
+                    </div>
+                  ))}
+              </div>
+            </div>
           ) :
             console.log()
         }
