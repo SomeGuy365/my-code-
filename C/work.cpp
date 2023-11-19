0631681a577cb3ab7b8552b0089f17f211303bcb
Ethan denton <ethanjdenton@gmail.com>
2023-11-19 16:21:11 +0000

    Giant WIP

--- C/work.cpp
+++ C/work.cpp
@@ -1,23 +1,52 @@
 #include <iostream>
+#include <list>
 
 using namespace std;
 
+
 int main() {
-    string input1 = "please";
-    int num1 = 0;
-    int num2 = 0;
-    cout << "what you wanna do>" << endl << "1:add" << endl << "2:subtract" << endl << "3:multiply" << endl;
-    cin >> input1;
-    cout << "enter num 1:";
-    cin >> num1;
-    cout << "enter num 2:";
-    cin >> num2;
-    if (input1 == "1") {
-        cout << num1+num2 << endl;
-    } else if (input1 == "2") {
-        cout << num1-num2 << endl;
-    } else if (input1 == "3") {
-        cout << num1*num2 << endl;
+    string input;
+    int pasnum;
+    int num1,num2;
+    list<char> operators{};
+    cout << "Enter you equation:";
+    cin >> input;
+    for (auto i:input) {
+        operators.push_back(i);
     };
+
+    int n = size(operators);
+    list<char>::iterator its = operators.begin();
+    list<char>::iterator itm = operators.begin();
+    list<char>::iterator ite = operators.begin();
+    advance(itm,1);
+    advance(ite,2);
+    
+    for (int b = 0;b < n+1;b++) {
+
+        switch(*itm)
+        {
+        case '+':
+            cout << endl << "addition" << endl << *its << endl << *itm << endl << *ite;
+            num1 = *its - '0';
+            num2 = *ite - '0';
+            break;
+        case '-':
+            cout << "subtract";
+            break;
+        default:
+            break;
+        }
+
+        advance(its,2);
+        advance(itm,2);
+        advance(ite,2);
+    }
+
+    cout << "Output:";
+    for (auto c:operators) {
+        cout << c;
+    }
+    
     return 0;
 }
